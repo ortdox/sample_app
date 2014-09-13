9a5e9ed423e6d48400630755d4a3d2415d486490
ortdox <myortdox@gmail.com>
2014-09-13 11:20:37 +0400

    fix controller 2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,10 @@
 class StaticPagesController < ApplicationController
-<<<<<<< HEAD
-
-=======
->>>>>>> static-pages
   def home
   end
 
   def help
   end
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> static-pages
   def about
   end
 end
